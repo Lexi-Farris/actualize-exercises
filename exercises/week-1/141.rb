c5513006be08fcc585c5dcb9642ba689dba32587
Lexi <mlexifarris@gmail.com>
2023-09-17 16:22:55 -0500

    Week 4 Exercises

---
+++ exercises/week-1/141.rb
@@ -0,0 +1,20 @@
+# QUESTION 1
+# Finish annotating the code below.
+x = 0                                  # 1
+names = ["Saron", "Majora", "Danilo"]  # 2
+names[1] = "Bob"                       # 3
+x = names[2]                           # 4
+names[0] = x                           # 5
+pp names                               # 6
+
+# 1: x is 0
+# 2: x is 0, names is ["Saron", "Majora", "Danilo"]
+# 3: Change the 2nd item "Majora" to 'Bob'
+# 4: Change the value of the variable, X to the 3rd item in the array 'Danilo' 
+# 5: Change 'Saron' (value 1) to 'Danilo' (value 3)
+# : prints the values in the array 'names'
+
+# QUESTION 2
+# Find and fix the mistakes in the code below.
+items = [3, 2, 3, 2]
+pp items
