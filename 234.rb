ceb9b3aa7fec7ea3586bc6757820ba6e1439593e
Lexi <mlexifarris@gmail.com>
2023-09-21 15:26:25 -0500

    Add exercise solutions

--- 234.rb
+++ 234.rb
@@ -1,14 +1,14 @@
 # QUESTION 1
 # Finish annotating the code below (just mark the lines that get executed). 
 # The first several comments are done for you.
-booyeah = 0                # 1     10
-2.times do                 # 2     11
-  booyeah = booyeah + 1    # 3     12
-  3.times do               # 4 6 8 
-    booyeah = booyeah + 2  # 5 7 9 
+booyeah = 0                # 1     
+2.times do                 # 2     10
+  booyeah = booyeah + 1    # 3     11
+  3.times do               # 4 6 8 12 14 16
+    booyeah = booyeah + 2  # 5 7 9 13 15 17
   end                      
 end                                 
-p booyeah                  #        13
+p booyeah                  #                  18
 
 #1 (booyeah = 0) 
 #2 'do line 6 2 times AFTER going through the code on line 8'
@@ -19,9 +19,15 @@ p booyeah                  #        13
 #7 (booyeah = 5)
 #8 last time doing line 8
 #9 (booyeah = 7)
-#10 do line 6 1 more time
+#10 do line this loop 1 last time
 #11 (booyeah =8) 
-#12 prints value of booyeah (8)
+#12 run the code below 3 times
+#13 (booyeah =10 )
+#14 run the code below 2  times
+#15 (booyeah = 12)
+#16 run the code below for the last time
+#17 (booyeah= 14 )
+#18 print 14
 
 
 #QUESTION 2
