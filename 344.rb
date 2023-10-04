230603bbe404d390b0f2ce6646f5f42aa3e3ba3f
Lexi <mlexifarris@gmail.com>
2023-10-04 11:37:20 -0500

    added disco problems

--- 344.rb
+++ 344.rb
@@ -4,27 +4,16 @@
 # (1 is I, 2 is II, 3 is III, 4 is IV, 5 is V, 6 is VI, 7 is VII, etc.)
 # (You can see an example of all the rules here: https://byjus.com/maths/roman-numerals/#chart)
 
-Write a method with conditional that takes in a number. If it is a 5 then turn it into a V
-Write a method that takes in a number and returns "I" times the number (3 => "III")
-
-
-
-
+1. Create hash that breaks down code 1 ==> I and so on
+2. hash should return the value when key is given
+3. Write a method with conditional that takes in a number. If it is a 5 then turn it into a V
+4. Write a method that takes in a number and returns "I" times the number (3 => "III")
 
 =end
 
+n = 5
 
-class Roman_num_converter  
- attr_reader :number
-  
- def initialize(number)
-   @number = number
-
-  end
+if n == 5
+  puts "V"
 end
-
-
-
-
-
 
